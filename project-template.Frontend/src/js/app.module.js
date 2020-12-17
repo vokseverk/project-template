b84e49b14cd3466303593616fab7147f5169ffed
Chriztian Steinmeier <chriztian@steinmeier.dk>
2020-12-18 00:50:15 +0100

    Fix a couple of typos

--- project-template.Frontend/src/js/app.module.js
+++ project-template.Frontend/src/js/app.module.js
@@ -1,7 +1,7 @@
 //
 //	This is the "module" version of the app's main JS file.
-//	It's purely a wrapper file — code your app in the app.js file'
-//	references below along with various modules beside it.
+//	It's purely a wrapper file — code your app in the app.js file
+//	referenced below along with various modules beside it.
 //
 
 import inititalize from './modules/app.js'
--- project-template.Frontend/src/js/app.nomodule.js
+++ project-template.Frontend/src/js/app.nomodule.js
@@ -3,8 +3,8 @@
 //	This file should be transpiled and bundled by CodeKit as a
 //	fallback file for browsers that don't support JS modules.
 //	
-//	It's purely a wrapper file — code your app in the app.js file'
-//	references below along with various modules beside it.
+//	It's purely a wrapper file — code your app in the app.js file
+//	referenced below along with various modules beside it.
 //
 
 import inititalize from './modules/app.js'
