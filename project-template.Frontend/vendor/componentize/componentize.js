263cffd6ec465a555fe77fc573139efe14815b82
Chriztian Steinmeier <chriztian@steinmeier.dk>
2022-03-19 23:55:37 +0100

    Update componentize.js

--- project-template.Frontend/vendor/componentize/componentize.js
+++ project-template.Frontend/vendor/componentize/componentize.js
@@ -4,66 +4,66 @@
 	function createModifierSwitches(componentWrapper) {
 		const togglesElement = document.createElement('fieldset');
 		togglesElement.classList.add('component-states-modifiers');
-		
+
 		const legend = document.createElement('legend');
 		legend.textContent = 'States & Modifiers';
-		
+
 		togglesElement.appendChild(legend);
-		
-		const identifier = componentWrapper.dataset.title.toLowerCase().replace(/\s+/, '-');
-		
+
+		const identifier = getIdentifierFromName(componentWrapper.dataset.title);
+
 		const states = componentWrapper.dataset.states?.split(',');
 		const modifiers = componentWrapper.dataset.modifiers?.split(',');
-		
+
 		if (modifiers) {
 			const wrapper = document.createElement('div');
-			Array.from(modifiers).forEach((modifier) => {
+			modifiers.forEach((modifier) => {
 				const field = document.createElement('div');
 				const input = document.createElement('input');
 				input.type = 'checkbox';
 				input.name = `mod-${modifier}`;
 				input.value = modifier;
 				input.id = `${identifier}-mod-${modifier}`;
-				
+
 				const label = document.createElement('label');
 				label.htmlFor = input.id;
 				label.textContent = modifier;
-				
+
 				field.appendChild(input);
 				field.appendChild(label);
-				
+
 				wrapper.appendChild(field);
 			});
-			
+
 			togglesElement.appendChild(wrapper);
 		}
-		
+
 		if (states) {
 			const wrapper = document.createElement('div');
-			Array.from(states).forEach((state, index) => {
+			states.forEach((state, index) => {
 				const field = document.createElement('div');
 				const input = document.createElement('input');
 				input.type = 'radio';
-				input.name = 'component-state';
+				input.name = `${identifier}-state`;
 				input.value = state;
 				input.id = `${identifier}-state-${state}`;
 				input.checked = index == 0;
-				
+
 				const label = document.createElement('label');
 				label.htmlFor = input.id;
 				label.textContent = (state == 'nil' ? '(none)' : state);
-				
+
 				field.appendChild(input);
 				field.appendChild(label);
-				
+
 				wrapper.appendChild(field);
 			});
-			
+
 			togglesElement.appendChild(wrapper);
 		}
-		
+
 		setEventHandler(togglesElement);
-		
+
 		return togglesElement
 	}
 
@@ -74,13 +74,19 @@
 			const value = target.value;
 			const componentWrapper = element.parentNode;
 			const component = componentWrapper.firstElementChild;
-			
+
 			if (target.type === 'radio') {
 				setState(component, value);
 			} else {
 				component.classList.toggle(value);
 			}
 		});
+	}
+
+	function getIdentifierFromName(name) {
+		let identifier = name.toLowerCase().replaceAll(/\s+/g, '-');
+		identifier = identifier.replaceAll(/[()%&?]/g, '');
+		return identifier
 	}
 
 	function setState(element, newState) {
@@ -172,4 +178,4 @@
 		}
 	});
 
-}());
+})();
