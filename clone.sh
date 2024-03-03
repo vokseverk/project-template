bf30cc8519eb157a2b834ca909eebcd1a9f88e70
Chriztian Steinmeier <chriztian@steinmeier.dk>
2024-03-03 08:01:19 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -6,8 +6,16 @@ PROJECT_ALIAS=$1
 if [ ! "$PROJECT_ALIAS" = "" ]; then
 	git clone --origin LIVE "https://scm.umbraco.io/${REGION}/${PROJECT_ALIAS}.git" "${PROJECT_NAME}.Web"
 else
-	echo <<ENDDOC
+	cat <<ENDDOC
+
 You must specify the project's alias, e.g.:
-\$ source clone.sh my-project-alias
+
+\$ ./clone.sh my-project-alias
+
+If you need to clone from another region than the default (euwest01), you can
+specify that as well, e.g.:
+
+\$ ./clone.sh my-project-alias uswest01
+
 ENDDOC
 fi
