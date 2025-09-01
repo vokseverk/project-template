175660674e909ffbafc85ab9dc33b084b6eb41d7
Chriztian Steinmeier <greystate@users.noreply.github.com>
2025-09-01 19:00:27 +0200

    fix: prompting for password and Umbraco version

--- create-umbraco.sh
+++ create-umbraco.sh
@@ -2,16 +2,19 @@ CURRENT_DIR=`pwd`
 PROJECT_NAME=`basename "$CURRENT_DIR"`
 
 if [ -e "${PROJECT_NAME}.Web" ]; then
-	read -p "Enter password for the admin user (min. 10 chars): " PASSWORD
+	cd $PROJECT_NAME.Web
+
+	echo "Enter password for the admin user (min. 10 chars): "
+	read PASSWORD
 
 	if [[ $PASSWORD == "" ]]; then
 		exit 1
 	else
-
-		dotnet new install Umbraco.Templates --force
+		dotnet new install Umbraco.Templates::13.10.0 --force
 
 		dotnet new umbraco --force -n "UmbracoProject" --friendly-name "Administrator" --email "umbracoadmin@vokseverk.dk" --password "${PASSWORD}" --development-database-type SQLite
 
+		cd ..
 		echo "New Umbraco website created in $PROJECT_NAME.Web/UmbracoProject"
 	fi
 else
